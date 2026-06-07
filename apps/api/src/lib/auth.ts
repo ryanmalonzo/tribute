d79bc73b933ccfb98ec89d9ce96dad2c1bc3cafd
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-06-07 20:54:17 +0200

    feat(api): create authentication handler
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { betterAuth } from "better-auth/minimal";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 
 const adapter = new PrismaPg({
 	connectionString: process.env.DATABASE_URL,
