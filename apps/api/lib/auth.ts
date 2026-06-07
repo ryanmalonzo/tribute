2ee6487feee5980c252e619e1d1e95c4ebebbbe4
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2026-06-07 20:49:42 +0200

    feat(api): create better-auth tables
    Signed-off-by: Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>

---
+++ apps/api/lib/auth.ts
@@ -0,0 +1,17 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { betterAuth } from "better-auth/minimal";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaPg({
+	connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+});
